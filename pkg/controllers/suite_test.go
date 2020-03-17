dda3e0898a8827793fcd9e452fdd6d6d6ef368df
Felix Furrer <felix.furrer@daimler.com>
2020-03-17 08:14:07 +0100

    Refactor test suite

--- pkg/controllers/suite_test.go
+++ pkg/controllers/suite_test.go
@@ -11,59 +11,19 @@ import (
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 
-	ginkgoconfig "github.com/onsi/ginkgo/config"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"github.com/onsi/ginkgo/config"
 )
-
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
-
-	/* RunSpecsWithDefaultAndCustomReporters(t,
-	"Controller Suite",
-	[]Reporter{envtest.NewlineReporter{}}) */
-
-	resultDir, present := os.LookupEnv("TEST_RESULT_DIR")
-	if !present {
-		resultDir = "build/test-results"
+	resultDir := os.Getenv("TEST_RESULT_DIR")
+	if resultDir == "" {
+		resultDir = "../../build/test-results"
 	}
-	_ = os.MkdirAll(resultDir, os.ModePerm)
+	if err := os.MkdirAll(resultDir, os.ModePerm); err != nil {
+		panic(err)
+	}
 	junitReporter := reporters.NewJUnitReporter(filepath.Join(resultDir, "tests-junit-report.xml"))
-	ginkgoconfig.DefaultReporterConfig.Verbose = true
-	RunSpecsWithDefaultAndCustomReporters(t, "Controller Suite", []Reporter{junitReporter})
+	config.DefaultReporterConfig.Verbose = true
+	RunSpecsWithDefaultAndCustomReporters(t, "Controllers Suite", []Reporter{junitReporter})
 }
-
-// var _ = BeforeSuite(func(done Done) {
-// 	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
-
-// 	By("bootstrapping test environment")
-// 	testEnv = &envtest.Environment{
-// 		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
-// 	}
-
-// 	cfg, err := testEnv.Start()
-// 	Expect(err).ToNot(HaveOccurred())
-// 	Expect(cfg).ToNot(BeNil())
-
-// 	err = corev1.AddToScheme(scheme.Scheme)
-// 	Expect(err).NotTo(HaveOccurred())
-
-// 	// +kubebuilder:scaffold:scheme
-
-// 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-// 	Expect(err).ToNot(HaveOccurred())
-// 	Expect(k8sClient).ToNot(BeNil())
-
-// 	close(done)
-// }, 60)
-
-// var _ = AfterSuite(func() {
-// 	By("tearing down the test environment")
-// 	err := testEnv.Stop()
-// 	Expect(err).ToNot(HaveOccurred())
-// })
